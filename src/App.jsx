5c39805ec67052917837ccb88c7d510ddca9ca0c
Muhammad Ghulam Ali <mghulamali888@gmail.com>
2026-01-19 11:34:10 +0500

    Fix Footer import casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import Header from "./components/Header"
-import Footer from "./components/footer"
+import Footer from "./components/Footer"
 
 // Pages
 import Home from "./pages/Home"
